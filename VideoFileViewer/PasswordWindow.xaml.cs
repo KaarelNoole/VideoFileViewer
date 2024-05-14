94408d882c65d47d7724959948c32de12fcb7ec7
KaarelNoole <kaarelnoole@gmail.com>
2024-05-14 11:19:11 +0300

    Add project files.

---
+++ VideoFileViewer/PasswordWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using VideoOS.Platform.UI.Controls;
+
+namespace VideoFileViewer
+{
+    public partial class PasswordWindow : VideoOSWindow
+    {
+        public string Password => _passwordTextBox.Password;
+
+        public PasswordWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
