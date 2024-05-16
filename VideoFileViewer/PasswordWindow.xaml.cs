45ff703dd0fd1e4075b24587f67fdd6b548bf02d
KaarelNoole <kaarelnoole@gmail.com>
2024-05-16 10:08:44 +0300

    Fix PasswordWindow

--- VideoFileViewer/PasswordWindow.xaml.cs
+++ VideoFileViewer/PasswordWindow.xaml.cs
@@ -1,26 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
-using VideoOS.Platform.UI.Controls;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace VideoFileViewer
 {
-    public partial class PasswordWindow : VideoOSWindow
+    public partial class PasswordWindow : Window
     {
-        string password;
         public PasswordWindow()
         {
             InitializeComponent();
         }
 
-        private void InitializeComponent()
-        {
-            throw new NotImplementedException();
-        }
-
         public string Password
         {
-            get { return password; }
+            get { return textBoxPassword.Password; }
         }
+
         private void OkButton_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = true;
